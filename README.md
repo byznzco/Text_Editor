38bff3b9e63302ec4c408e51122ea9c961cb87c1
byznzco <85300745+byznzco@users.noreply.github.com>
2022-11-30 16:55:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Text_Editor
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vmbt6k)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// bold, italic, undo, redo
+let optionsButtons = document.querySelectorAll(".option-button");
+
+// h1,h2,h3,h4,h4,h6 .adv-option-button
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+
+// fontName
+let fontName = document.getElementById("fontName")
+
+// writing area  text-input
+let writingArea = 
